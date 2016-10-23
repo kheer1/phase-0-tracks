cfe1a96ed44d951696b81c2c4686e716c771be62
Kamal Heer <kheer1@gmail.com>
2016-10-23 17:49:52 -0500

    Updated the shout module to a mixin version

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,34 @@
 module Shout
-	def self.yell_angrily(words)
+	#def self.yell_angrily(words)
+	def yell_angrily(words)
     	words + "!!!" + " :("
   	end
 
-  	def self.yell_happily(words)
+  	#def self.yell_happily(words)
+  	def yell_happily(words)
   		words + "!!!!!!!!" + " 8-D"
 	end
 
 end #end of Shout module
 
-puts Shout.yell_happily("Hello")
-puts Shout.yell_angrily ("Good Bye")
+class Old_People
+	include Shout
+end
+
+class Kids
+	include Shout
+end
+
+#drive code
+puts Old_People.new.yell_angrily("Damn kids")
+
+kid=Kids.new
+
+puts kid.yell_happily("What's for dinner")
+
+
+
+
+
+#puts Shout.yell_happily("Hello")
+#puts Shout.yell_angrily ("Good Bye")
