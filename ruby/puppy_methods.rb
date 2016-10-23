dfaf88325975fec857cc555006c0b27b14bd4a30
Kamal Heer <kheer1@gmail.com>
2016-10-23 01:14:28 -0500

    created my own class called Car

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,11 +32,40 @@ class Puppy
 
 end
 
+class Car
+
+	def	initialize
+		puts "Starting up the car!!!"
+	end
+
+	def model
+		puts "BMW"
+	end
+
+	def breaking (speed)
+		stop = 0
+		
+		until stop >= speed
+			puts "My speed is #{speed}"
+			speed = speed/2
+		end
+		puts "Finally Stopped!!"
+	end
+
+end
+
+
+
 #drive code
 spot = Puppy.new
 
+puts spot.respond_to?(:fetch) ? spot.fetch("bone") : "It doesn't know how to fetch"
 spot.fetch("ball")
 spot.speak(3)
 spot.roll_over
 puts spot.dog_years(4)
 spot.sit
+
+betty=Car.new
+betty.model
+betty.breaking(100)
