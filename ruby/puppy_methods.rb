a20313b5ce44c448b1dce332a5e8a5ce0a23ac39
Kamal Heer <kheer1@gmail.com>
2016-10-23 00:50:25 -0500

    created four methods for puppy_method class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,36 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+  	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+  	end
+
+	def speak (number)
+		start = 1
+		
+		until start > number
+			puts "Woof!"
+			start +=1
+		end
+	end
+
+	def roll_over
+		puts "*rolls over*"
+	end
+
+	def dog_years (number)
+		number = 7 * number
+	end
+
+	def sit
+		puts "*sits down*"
+	end
 
 end
 
+spot = Puppy.new
+
+spot.fetch("ball")
+spot.speak(3)
+spot.roll_over
+puts spot.dog_years(2)
