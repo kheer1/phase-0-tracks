1e85d0b44cae5ded3613321000d04676a0cc7b7b
Kamal Heer <kheer1@gmail.com>
2016-10-23 00:18:10 -0500

    Puppy class with comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#Species -----------------
+#Dog
+
+#Characteristies ---------
+#Eyes: 2
+#Ears: 2
+#Legs: 4
+#Tails: 1
+#Color: varies
+
+#Behavior ---------------
+#Barks
+#Playful
+#Fast
+#Loyal
