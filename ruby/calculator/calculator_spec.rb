4cfa3e6eab1a59ad19d1e059e512bac3bea7b1db
Kamal Heer <kheer1@gmail.com>
2016-10-23 18:50:42 -0500

    updated calculator file and rspec file

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -10,18 +10,23 @@
 
 require_relative 'calculator'
 
-describe Calculator do
-  let(:calculator) { Calculator.new }
+  describe Calculator do
+      let(:calculator) { Calculator.new }
 
-  it "adds two integers" do
-    expect(calculator.add(3,4)).to eq 7
+      it "adds two integers" do
+          expect(calculator.add(3,4)).to eq 7
+      end
+
+      it "subtracts two integers" do
+          expect(calculator.subtract(7,1)).to eq 6
+      end
+
+      it "multiplies two integers" do
+          expect(calculator.multiply(2,3)).to eq 6
+      end
+
+      it "divide two integers" do
+          expect(calculator.divide(15,3)).to eq 5
+      end
   end
 
-  it "subtracts two integers" do
-    expect(calculator.subtract(7,1)).to eq 6
-  end
-
-  it "multiplies two integers" do
-    expect(calculator.multiply(2,3)).to eq 6
-  end
-end
